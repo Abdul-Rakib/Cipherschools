3fe1a7ec583b8d7b5b51b0d07b3050f0ec818075
abdulrakib <firefalls2004@gmail.com>
2023-10-08 23:32:29 +0530

    Create Lecture 6 II-CipherSchools.cpp

---
+++ Lecture 6 II-CipherSchools.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int arr[100];
+    // print number 1to 100 using for loop
+    
+    for(int i=0;i<100;i++){
+        arr[i]=i;
+        cout<<arr[i]<<" ";
+    }
+}
